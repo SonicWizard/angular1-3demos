77a634cade5bcfe0513c742b38af03ba8f382fc6
Darren Nelsen <dnelsen@amplify.com>
2014-11-17 16:35:02 -0500

    build out basic form

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 var app = angular.module('myApp', ['ngRoute']);
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/forms', {
+      templateUrl: 'partials/forms.html',
+      controller: 'FormsCtrl'
+  });
+  $routeProvider.otherwise({redirectTo: '/view1'});
+}]);
---
+++ js/controllers.js
@@ -0,0 +1,5 @@
+angular.module('myApp')
+.controller('FormsCtrl', ['$scope', function($scope) {
+    $scope.first = 'Mickey';
+    $scope.last = 'Mouse';
+}]);
