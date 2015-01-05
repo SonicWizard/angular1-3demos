63ada8aad832b291e0fbb213739eb88dc888a36f
Darren Nelsen <dnelsen@amplify.com>
2015-01-04 21:34:14 -0500

    Added ngMessages demo; cleaned up one-time binding demo

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
-var app = angular.module('myApp', ['ngRoute']);
+var app = angular.module('myApp', ['ngRoute', 'ngAria', 'ngMessages']);
 app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/forms', {
-      templateUrl: 'partials/forms.html',
-      controller: 'FormsCtrl'
-  });
+  $routeProvider
+    .when('/onetimebinding', {
+        templateUrl: 'partials/onetimebinding.html',
+        controller: 'OneTimeBindingCtrl'
+    })
+    .when('/aria', {
+        templateUrl: 'partials/aria.html',
+        controller: 'AriaCtrl'
+    })
+    .when('/ngMessages', {
+        templateUrl: 'partials/ngMessages.html',
+        controller: 'NgMessagesCtrl'
+    })
+    .when('/forms', {
+        templateUrl: 'partials/forms.html',
+        controller: 'FormsCtrl'
+    });
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,21 @@
 angular.module('myApp')
+.controller('OneTimeBindingCtrl', ['$scope', function($scope) {
+    $scope.first = 'Mickey';
+    $scope.last = 'Mouse';
+    $scope.boundText = "the text";
+}])
+.controller('AriaCtrl', ['$scope', function($scope) {
+    $scope.disabled = false;
+    $scope.btnText = 'Disable';
+    $scope.toggle = function() {
+        disabled = !disabled;
+        btnText = disabled ? "Enable" : "Disable";
+    };
+}])
+.controller('NgMessagesCtrl', ['$scope', function($scope) {
+    $scope.field;
+    $scope.field2;
+}])
 .controller('FormsCtrl', ['$scope', function($scope) {
     $scope.first = 'Mickey';
     $scope.last = 'Mouse';
